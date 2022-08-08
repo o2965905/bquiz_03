47f43e6c05d28387b73175322f3b755ce669b1f2
o2965905 <o2965905@gmail.com>
2022-08-08 09:11:39 +0800

    完成後台預告片資料編輯/刪除功能

--- api/edit_poster.php
+++ api/edit_poster.php
@@ -1,1 +1,32 @@
 <!-- 撰寫海報的編輯片名/顯示/刪除/動畫等功能 -->
+<?php
+include_once "../base.php";
+
+foreach($_POST['id'] as $key => $id){
+
+    //判斷是否有資料要刪除的
+     if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        
+        $Poster->del($id);//刪除指定id的資料
+
+    }else{
+        //如果不是刪除,就是要編輯該筆資料
+        $row=$Poster->find($id);
+        $row['name']=$_POST['name'][$key];
+        $row['ani']=$_POST['ani'][$key];
+
+
+        //判斷$_POST['sh']陣列中是否有指定id的值，
+        //有則表示要設定為顯示 1
+        //無則表示要設定為隱藏 0
+        $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+
+
+
+        $Poster->save($row);
+    }
+}
+
+
+to("../back.php?do=poster");
+?>
