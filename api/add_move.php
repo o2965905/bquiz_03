ff1854ab7d6d459aea27ed15dcb3acc994572ab4
o2965905 <o2965905@gmail.com>
2022-08-08 13:34:07 +0800

    製作後台院線片列表

--- api/add_move.php
+++ api/add_move.php
@@ -12,8 +12,15 @@ if(isset($_FILES['poster']['tmp_name'])){
     move_uploaded_file($_FILES['poster']['tmp_name'],'../upload/'.$_FILES['poster']['name']);
 }
 
+//把三個日期資料以符號'-'連成一個日期字串
 $_POST['ondate']=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
+
+//完成日期字串後，再把三個日期變數從$_POST陣列中刪除，
+//目的是確保$_POST陣列中的資料符合資料表的欄位
 unset($_POST['year'],$_POST['month'],$_POST['day']);
+
+//預設的排序值為資料的id值，因此使用函式去計算目前的最大id值
+//藉此來計算最大的排序值
 $_POST['sh']=1;
 $_POST['rank']=$Movie->math('max','id')+1;
 
--- back/movie.php
+++ back/movie.php
@@ -1,3 +1,35 @@
 <!-- 院線片管理 -->
 <button onclick="location.href='?do=add_move'">新增電影</button>
 <hr>
+<div style="overflow:scroll;height:430px;">
+
+    <div style="background:#eee;width:99%;height:140px;margin:2px 0;display:flex;">
+        <div style="width:15%;">
+            <img src="" alt=""> 123
+        </div>
+        <div style="width:15%;">
+            分級: <img class="img-fluid" src="" alt="">
+        </div>
+        <div style="width:70%;">
+            <div style="display:flex;">
+                <div style="width:33.33%;">片名:</div>
+                <div style="width:33.33%;">片長:</div>
+                <div style="width:33.33%;">上映時間:</div>
+            </div>
+            <div>
+                <button>顯示</button>
+                <button>往上</button>
+                <button>往下</button>
+                <button>編輯電影</button>
+                <button>刪除電影</button>
+            </div>
+            <div>
+                劇情介紹:
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+
+</script>
