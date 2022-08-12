4d35d993ad89e8446644ae0595e8eb7500b24e02
o2965905 <o2965905@gmail.com>
2022-08-12 10:43:25 +0800

    建立電影訂票管理畫面及刪除單筆清單的功能

--- back/order.php
+++ back/order.php
@@ -1,2 +1,42 @@
 <!-- 電影訂票管理 -->
-電影訂票管理
+<h3 class="ct">訂單清單</h3>
+
+<div class="header" style="display:flex;">
+    <div>訂單編號</div>
+    <div>電影名稱</div>
+    <div>日期</div>
+    <div>場次時間</div>
+    <div>訂購數量</div>
+    <div>訂購位置</div>
+    <div>操作</div>
+</div>
+<div style="overflow:auto;height: 450px;">
+    <?php
+    $orders = $Order->all();
+    foreach ($orders as $ord) {
+    ?>
+        <div style="display:flex;">
+            <div><?= $ord['no']; ?></div>
+            <div><?= $ord['movie']; ?></div>
+            <div><?= $ord['date']; ?></div>
+            <div><?= $ord['session']; ?></div>
+            <div><?= $ord['qt']; ?></div>
+            <div><?= $ord['seats']; ?></div>
+            <div><button onclick="del('orders',<?= $ord['id']; ?>)">刪除</button></div>
+        </div>
+        <hr>
+    <?php
+    }
+    ?>
+</div>
+<script>
+    //刪除電影按鈕功能
+    function del(table, id) {
+        $.post("./api/del.php", {
+            table,
+            id
+        }, () => {
+            location.reload();
+        })
+    }
+</script>
