7c73b0e11430b28958fd4c8b9fc680b00214c500
o2965905 <o2965905@gmail.com>
2022-08-16 15:47:09 +0800

    完成訂票頁面設及取得電影列表及日期列表函式

---
+++ api/movie_list.php
@@ -0,0 +1,16 @@
+<!-- 前台線上訂票，撈電影的資料 -->
+
+<?php
+
+include_once "../base.php";
+
+$selectId=$_POST['id'];
+$startDate=date("Y-m-d",strtotime("-2 days"));
+$today=date("Y-m-d");
+$movies=$Movie->all(" Where `sh`='1' && ondate between '$startDate' AND '$today'");
+
+foreach($movies as $movie){
+    $selected=($selectId==$movie['id'])?'selected':'';
+    echo "<option value='{$movie['id']}' $selected>{$movie['name']}</option>";
+}
+?> 
--- front/order.php
+++ front/order.php
@@ -1,2 +1,63 @@
-<!-- 訂票頁面 -->
-訂票<?=$_GET['id']??0;?>
+<?php  $selectedMovieId=$_GET['id']??0 ;?>
+
+<h2 class="ct">線上訂票</h2>
+
+<table id="order" style="width:500px;margin:auto">
+    <tr>
+        <td width="15%">電影：</td>
+        <td>
+            <select name="movie" id="movie"></select>
+        </td>
+    </tr>
+    <tr>
+        <td>日期：</td>
+        <td>
+        <select name="date" id="date"></select>
+        </td>
+    </tr>
+    <tr>
+        <td>場次：</td>
+        <td>
+        <select name="session" id="session"></select>
+        </td>
+    </tr>
+</table>
+<div class="ct">
+    <button>確定</button>
+    <button>重置</button>
+</div>
+
+<script>
+    $("#movie").load("./api/movie_list.php",{id:<?=$selectedMovieId;?>},()=>{
+        let movie=$("#movie").val();
+        getDate(movie)
+    })
+
+    $("#movie").on('change',function(){
+        let movie=$(this).val();
+        getDate(movie)
+
+    })
+
+    $("#date").on('change',function(){
+        let movie=$("#movie").val();
+        let date=$(this).val();
+        getSession(movie,date)
+    })
+
+    function getDate(movie){
+
+        $("#date").load("./api/date_list.php",{movie},()=>{
+            let date=$("#date").val()
+            getSession(movie,date)
+        })
+    }
+
+    function getSession(movie,date){
+        console.log(movie,date);
+        $("#session").load("./api/session_list.php",{movie,date},()=>{
+
+        })
+    }
+
+</script> 
